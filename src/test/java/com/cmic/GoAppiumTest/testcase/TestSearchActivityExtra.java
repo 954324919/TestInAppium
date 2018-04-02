89d7c62735f7bbedea027265225ccadcd53c328c
kiwi <13802885425@139.com>
2018-04-03 01:34:58 +0800

    [unstable-feau]搜索联想页面测试使用坐标定位自动联想控件

--- src/test/java/com/cmic/GoAppiumTest/testcase/TestSearchActivityExtra.java
+++ src/test/java/com/cmic/GoAppiumTest/testcase/TestSearchActivityExtra.java
@@ -14,6 +14,7 @@ import com.cmic.GoAppiumTest.base.DriverManger;
 import com.cmic.GoAppiumTest.helper.PageRedirect;
 import com.cmic.GoAppiumTest.helper.Tips;
 import com.cmic.GoAppiumTest.util.ContextUtil;
+import com.cmic.GoAppiumTest.util.KeyboardUtil;
 import com.cmic.GoAppiumTest.util.LogUtil;
 import com.cmic.GoAppiumTest.util.ScreenUtil;
 import com.cmic.GoAppiumTest.util.WaitUtil;
@@ -21,10 +22,16 @@ import com.cmic.GoAppiumTest.util.WaitUtil;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
-public class TestSearchActivityRelation {
+/**
+ * 包含搜索页面的一些非常规测试，包括联想和搜索结果，这两者结合起来测试比较方便，故放在一起
+ * 
+ * @风险 0403发现AutoCompleteTextView无法定位，考虑使用坐标定位，及其不稳定
+ * @author kiwi
+ */
+public class TestSearchActivityExtra {
 	private String mTag;
 	private AndroidDriver<AndroidElement> mDriver;
-	
+
 	@BeforeMethod
 	public void tipBeforeTestCase() {
 		// 点击同意并使用
@@ -57,18 +64,47 @@ public class TestSearchActivityRelation {
 		assertEquals(ContextUtil.getCurrentActivity(), ".activity.SearchActivity");
 		ScreenUtil.screenShot("进入必备应用搜索界面");
 	}
-	
 
-	@Test(dependsOnMethods = { "initCheck" })
+	@Test(dependsOnMethods = { "initCheck" }, timeOut = 15000)
 	@Tips(description = "热搜联想")
 	public void checkSearchRalation() throws InterruptedException {
 		LogUtil.printCurrentMethodName();
 		WaitUtil.forceWait(2);
 		AndroidElement et = mDriver.findElement(By.id("com.cmic.mmnes:id/searchText"));
-		et.click();//点击出现搜索联想
+		et.click();// 点击出现搜索联想
 		ScreenUtil.screenShot("联想搜索前...");
 		et.sendKeys("移动");
 		WaitUtil.forceWait(1500);
 		ScreenUtil.screenShot("联想搜索后...");
+		KeyboardUtil.hideKeyBoard();
+	}
+
+	@Test(dependsOnMethods = { "initCheck" }, timeOut = 15000)
+	@Tips(description = "点击下载", //
+			riskPoint = "由于自动补全控件无法定位，当前预期先使用坐标定位，待解决。不抛出异常，只做正向验证")
+	public void checkClickDownload() {
+		LogUtil.printCurrentMethodName();
+		AndroidElement element = mDriver.findElementByClassName("android.widget.ListView");
+		System.out.println(element.getCoordinates().toString());
+	}
+
+	@Test(dependsOnMethods = { "initCheck" })
+	public void checkClick2DetailByRalationItem() {
+		LogUtil.printCurrentMethodName();
+	}
+
+	@Test(dependsOnMethods = { "initCheck" })
+	public void checkSearchHistory() {
+		LogUtil.printCurrentMethodName();
+	}
+
+	@Test(dependsOnMethods = { "initCheck" })
+	public void checkEnterDetailFromHistory() {
+		LogUtil.printCurrentMethodName();
+	}
+
+	@Test(dependsOnMethods = { "initCheck" })
+	public void checkClearHistory() {
+		LogUtil.printCurrentMethodName();
 	}
 }
