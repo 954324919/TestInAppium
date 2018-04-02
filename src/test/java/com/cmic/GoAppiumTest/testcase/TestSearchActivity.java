e19b9ffafb33da4b5cb2ab01ff8d57cf040bb6fa
kiwi <13802885425@139.com>
2018-04-02 15:50:17 +0800

    [new feau]添加SearchActivity的几个测试用例

--- src/test/java/com/cmic/GoAppiumTest/testcase/TestSearchActivity.java
+++ src/test/java/com/cmic/GoAppiumTest/testcase/TestSearchActivity.java
@@ -21,14 +21,18 @@ import io.appium.java_client.android.AndroidElement;
 
 /**
  * 搜索页面比较浅，暂不考虑太过复杂的耦合的问题
+ * 
  * @author kiwi
  *
  */
 public class TestSearchActivity {
-	
+
 	private String mTag;
 	private AndroidDriver<AndroidElement> mDriver;
-	
+
+	private int originItemCount;
+	private int currentItemCount;
+
 	@BeforeMethod
 	public void tipBeforeTestCase() {
 		// 点击同意并使用
@@ -41,7 +45,7 @@ public class TestSearchActivity {
 	}
 
 	@BeforeClass
-	@Tips(description="假设已经在MainAct",riskPoint="耦合度暂不考虑，从MainTest完成进入")
+	@Tips(description = "假设已经在MainAct", riskPoint = "耦合度暂不考虑，从MainTest完成进入")
 	public void beforeClass() {
 		mTag = getClass().getSimpleName();
 		mDriver = DriverManger.getDriver();
@@ -63,5 +67,72 @@ public class TestSearchActivity {
 		WaitUtil.forceWait(2);
 		assertEquals(ContextUtil.getCurrentActivity(), ".activity.SearchActivity");
 		ScreenUtil.screenShot("进入必备应用搜索界面");
+	}
+
+	@Test(dependsOnMethods = { "initCheck" })
+	@Tips(description = "点击搜索ActionBar的后退")
+	public void checkBack() throws InterruptedException {
+		AndroidElement backIv = mDriver.findElement(By.id("com.cmic.mmnes:id/search_back_layout"));
+		backIv.click();
+		WaitUtil.forceWait(2);
+		assertEquals(ContextUtil.getCurrentActivity(), ".activity.MainActivity");
+		// 获取瞬时滚动热词用于checkWordFromOutside测试
+		String rollHotKeyWordUiSelector = "new UiSelector().className(\"android.widget.LinearLayout\").resourceId(\"com.cmic.mmnes:id/search_layout\")"
+				+ ".childSelector(new UiSelector().className(\"android.widget.TextView\"))";
+		System.out.println(rollHotKeyWordUiSelector);
+		AndroidElement rollHotKeyWordTv = mDriver.findElementByAndroidUIAutomator(rollHotKeyWordUiSelector);
+		System.out.println(rollHotKeyWordTv.getText());
+		// 再次点击返回SearchActivity
+		AndroidElement searchLayout = mDriver.findElement(By.id("com.cmic.mmnes:id/search_layout"));
+		searchLayout.click();
+		WaitUtil.forceWait(2);
+	}
+
+	@Test(dependsOnMethods = { "checkBack" })
+	@Tips(description = "被传入的热词")
+	public void checkWordFromOutside() {//
+		AndroidElement searchEt = mDriver.findElement(By.id("com.cmic.mmnes:id/searchText"));
+		System.out.println(searchEt.getText());
+	}
+
+	@Test
+	@Tips(description = "点击加载更多")
+	public void checkGetMore() throws InterruptedException {
+		originItemCount = mDriver.findElementsByClassName("android.widget.LinearLayout").size();
+		AndroidElement getMoreIv = mDriver.findElement(By.id("com.cmic.mmnes:id/tv_more"));
+		getMoreIv.click();
+		WaitUtil.forceWait(2);
+		currentItemCount = mDriver.findElementsByClassName("android.widget.LinearLayout").size();
+		assertEquals(originItemCount < currentItemCount, true);
+
+	}
+
+	@Test(dependsOnMethods = { "checkGetMore" })
+	@Tips(description = "点击收起更多")
+	public void closeTheWordList() throws InterruptedException {
+		originItemCount = mDriver.findElementsByClassName("android.widget.LinearLayout").size();
+		AndroidElement getMoreIv = mDriver.findElement(By.id("com.cmic.mmnes:id/tv_more"));
+		getMoreIv.click();
+		WaitUtil.forceWait(2);
+		currentItemCount = mDriver.findElementsByClassName("android.widget.LinearLayout").size();
+		assertEquals(currentItemCount, originItemCount);
+	}
+
+	@Test
+	@Tips(description = "热搜联想")
+	public void checkSearchRalation() {
+
+	}
+
+	@Test
+	@Tips(description = "点击随机的一个热词Item")
+	public void randomCheckOne() {
+
+	}
+
+	@Test
+	@Tips(description = "点击搜索栏目的clear图标")
+	public void clickTheClearSearchRly() {
+
 	}
 }
