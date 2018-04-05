6edd08ede66843d64b84fe3f8a91276f6d090f73
kiwi <13802885425@139.com>
2018-04-05 17:23:15 +0800

    [new feau]测试用例树基本完成

--- src/test/java/com/cmic/GoAppiumTest/testcase/TestDetailActivity.java
+++ src/test/java/com/cmic/GoAppiumTest/testcase/TestDetailActivity.java
@@ -1,9 +1,65 @@
 package com.cmic.GoAppiumTest.testcase;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.cmic.GoAppiumTest.App;
+import com.cmic.GoAppiumTest.base.DriverManger;
+import com.cmic.GoAppiumTest.helper.PageRedirect;
+import com.cmic.GoAppiumTest.helper.Tips;
+import com.cmic.GoAppiumTest.util.ContextUtil;
+import com.cmic.GoAppiumTest.util.ScreenUtil;
+import com.cmic.GoAppiumTest.util.WaitUtil;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-
+/**
+ * 详情页面
+ * @author kiwi
+ *
+ */
 public class TestDetailActivity {
 	private String mTag;
 	private AndroidDriver<AndroidElement> mDriver;
+	
+	@BeforeMethod
+	public void tipBeforeTestCase() {
+		// 点击同意并使用
+		System.out.println("测试用例[" + (++App.CASE_COUNT) + "]开始");
+	}
+
+	@AfterMethod
+	public void tipAfterTestCase() {
+		System.out.println("测试用例[" + (App.CASE_COUNT) + "]结束");
+	}
+
+	@BeforeClass
+	@Tips(description="从主页进入",riskPoint="主页未显示")
+	public void beforeClass() throws InterruptedException {
+		mTag = getClass().getSimpleName();
+		mDriver = DriverManger.getDriver();
+		// TODO 在没有卸载软件时，可能会报错
+		PageRedirect.redirect2MainActivity();
+		System.out.println("测试用例集[" + mTag + "]开始");
+	}
+
+	@AfterClass
+	public void afterClass() throws InterruptedException {// 执行一些初始化操作
+		System.out.println("测试用例集[" + mTag + "]结束");
+	}
+
+	@Test(enabled=false)
+	public void initCheck() {// 1
+		// TODO 后期需要确定是否为初次安装还是应用启动
+		// 先确认是否进入该页面
+		assertEquals(ContextUtil.getCurrentActivity(), ".activity.SearchActivity");
+		ScreenUtil.screenShot("进入必备详情页面");
+		WaitUtil.implicitlyWait(2);
+	}
+
 }
--- src/test/java/com/cmic/GoAppiumTest/testcase/TestWebviewADActivity.java
+++ src/test/java/com/cmic/GoAppiumTest/testcase/TestWebviewADActivity.java
@@ -1,9 +1,83 @@
 package com.cmic.GoAppiumTest.testcase;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.cmic.GoAppiumTest.App;
+import com.cmic.GoAppiumTest.base.DriverManger;
+import com.cmic.GoAppiumTest.helper.PageRedirect;
+import com.cmic.GoAppiumTest.helper.Tips;
+import com.cmic.GoAppiumTest.util.ContextUtil;
+import com.cmic.GoAppiumTest.util.PageRouteUtil;
+import com.cmic.GoAppiumTest.util.ScreenUtil;
+import com.cmic.GoAppiumTest.util.WaitUtil;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class TestWebviewADActivity {
 	private String mTag;
 	private AndroidDriver<AndroidElement> mDriver;
+
+	@BeforeMethod
+	public void tipBeforeTestCase() {
+		// 点击同意并使用
+		System.out.println("测试用例[" + (++App.CASE_COUNT) + "]开始");
+	}
+
+	@AfterMethod
+	public void tipAfterTestCase() {
+		System.out.println("测试用例[" + (App.CASE_COUNT) + "]结束");
+	}
+
+	@BeforeClass
+	@Tips(description = "假设已经入Setting&&未跳转到其他页面")
+	public void beforeClass() throws InterruptedException {
+		mTag = getClass().getSimpleName();
+		mDriver = DriverManger.getDriver();
+		// TODO 在没有卸载软件时，可能会报错
+		PageRedirect.redirect2MainActivity();
+		System.out.println("测试用例集[" + mTag + "]开始");
+	}
+
+	@AfterClass
+	public void afterClass() throws InterruptedException {// 执行一些初始化操作
+		System.out.println("测试用例集[" + mTag + "]结束");
+	}
+
+	@Test
+	public void initCheck() {// 1
+		// TODO 后期需要确定是否为初次安装还是应用启动
+		// 先确认是否进入该页面
+		assertEquals(ContextUtil.getCurrentActivity(), ".activity.MainActivity");
+		ScreenUtil.screenShot("进入必备应用主页广告界面");
+		WaitUtil.implicitlyWait(2);
+	}
+
+	@Test
+	public void checMainGameAdShow() {
+
+	}
+
+	@Test
+	public void checkMainGameAdContent() {
+
+	}
+
+	@Test
+	public void checkMainSoftAdShow() {
+
+	}
+
+	@Test
+	public void checkMainSoftAdContent() {
+
+	}
+
 }
